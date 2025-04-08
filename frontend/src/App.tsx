9d4a5197d8d2e2c617eb6a77c3d47f371e6538ce
Tasedit <tasedit@gmail.com>
2025-04-07 20:59:12 -0600

    added cookie libraries

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,16 +1,16 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
 import LoginPage from "./pages/LoginPage";
 import RegisterPage from "./pages/RegisterPage";
-import TestIndex from "./pages/TestIndex";
+// import TestIndex from "./pages/TestIndex";
 
 function App() {
   return (
     <>
       <Router>
         <Routes>
-          <Route path="/" element={<TestIndex />} />
+          {/* <Route path="/" element={<TestIndex />} /> */}
           <Route path="/login" element={<LoginPage />} />
           <Route path="/register" element={<RegisterPage />} />
         </Routes>
